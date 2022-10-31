0f28d6eefd859cda4d14ea39f37a9bc87704864b
uni51 <unieye51@gmail.com>
2022-10-31 09:08:59 +0900

    3-3 Reactにおける型

---
+++ src/components/ContainerSample.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type ContainerProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const Container = (props: ContainerProps): JSX.Element => {
+  const { title, children } = props;
+
+  return (
+    <div style={{ background: "red" }}>
+      <span>{title}</span>
+      {/* propsのchildrenを埋め込むと、このコンポーネントの開始タグと閉じタグで囲んだ要素を表示します。 */}
+      <div>{children}</div>
+    </div>
+  );
+};
+
+const Parent = (): JSX.Element => {
+  return (
+    <Container title="Hello">
+      <p>ここの部分が背景色で囲まれます。</p>
+    </Container>
+  );
+};
+
+export default Parent;
